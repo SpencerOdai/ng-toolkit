93881e177cc22f8045b6381ecc8ad4b41d333fd5
Spencer <spencerodai@gmail.com>
2021-04-04 13:32:18 +0100

    feat: add actions

--- projects/demo/src/app/pages/datatable/components/datatable/datatable.component.ts
+++ projects/demo/src/app/pages/datatable/components/datatable/datatable.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IDatatableConfig } from 'projects/ngx-toolkit/datatable/src/lib/types/config';
+import { map } from 'rxjs/operators';
 import { FirestoreService } from '../../../../shared/firestore/firestore.service';
 
 @Component({
@@ -16,16 +17,39 @@ export class DatatableComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.users = this.firebase.colWithIds$<any>('users');
+    this.users = this.firebase.colWithIds$<any>('users')
+      .pipe(
+        map(users =>
+          users.map(user => ({
+            'Display Name': user.displayName,
+            'Email': user.email,
+            'Photo': `<img class="rounded-circle img-thumbnail" src="${user.photoURL}">`
+          })
+        )
+      ));
     this.config = {
-      properties: ['id', 'displayName', 'email', 'createdAt'],
+      // properties: ['id', 'Display Name', 'Email', 'Photo'],
       allowSelection: true,
       classes: ['table-stripped', 'table-lg'],
       headerClasses: [],
       pagination: {
         maxPageSize: 10,
         startPage: 0
-      }
+      },
+      actions: [
+        {
+          event: ()=> {},
+          text: '<i class="fa fa-eye mx-2"></i>'
+        },
+        {
+          event: ()=> {},
+          text: '<i class="fa fa-edit mx-2"></i>'
+        },
+        {
+          event: ()=> {},
+          text: '<i class="fa fa-trash mx-2"></i>'
+        }
+      ]
     };
   }
 
---
+++ projects/ngx-toolkit/dialog/src/lib/dialog.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-dialog',
+  template: `
+    <p>
+      dialog works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class DialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
